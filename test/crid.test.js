eff722996e1f90fe7cb92d7cc9032e1d51c634d4
JoaoPedro <jpmartinsf27@gmail.com>
2025-07-07 17:17:05 -0300

    [fix] Commented older tests out

--- test/crid.test.js
+++ test/crid.test.js
@@ -1,24 +1,24 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+// const { expect } = require("chai");
+// const { ethers } = require("hardhat");
 
-describe("UFRJ_CRID", () => {
-  let contract, admin, student;
+// describe("UFRJ_CRID", () => {
+//   let contract, admin, student;
 
-  before(async () => {
-    [admin, student] = await ethers.getSigners();
-    const UFRJ_CRID = await ethers.getContractFactory("UFRJ_CRID");
-    contract = await UFRJ_CRID.deploy();
-  });
+//   before(async () => {
+//     [admin, student] = await ethers.getSigners();
+//     const UFRJ_CRID = await ethers.getContractFactory("UFRJ_CRID");
+//     contract = await UFRJ_CRID.deploy();
+//   });
 
-  it("Should create courses only by admin", async () => {
-    await contract.connect(admin).createCourse("MAT1234", 30);
-    await expect(contract.connect(student).createCourse("INF1234", 20))
-      .to.be.revertedWith("Somente administrador");
-  });
+//   it("Should create courses only by admin", async () => {
+//     await contract.connect(admin).createCourse("MAT1234", 30);
+//     await expect(contract.connect(student).createCourse("INF1234", 20))
+//       .to.be.revertedWith("Somente administrador");
+//   });
 
-  it("Should enforce state transitions", async () => {
-    await contract.connect(student).updateEnrollment("MAT1234", 1); // CONFIRMADA
-    const enrollment = await contract.enrollments(student.address, "MAT1234");
-    expect(enrollment.state).to.equal(1); // Confirmada
-  });
-});
+//   it("Should enforce state transitions", async () => {
+//     await contract.connect(student).updateEnrollment("MAT1234", 1); // CONFIRMADA
+//     const enrollment = await contract.enrollments(student.address, "MAT1234");
+//     expect(enrollment.state).to.equal(1); // Confirmada
+//   });
+// });
